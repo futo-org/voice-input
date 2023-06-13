e09918a5b46534407a649b2fa1a6804223c6e864
Aleksandras Kostarevas <aleks076@protonmail.com>
2023-06-13 12:48:15 +0300

    Add simple onboarding setup

--- app/src/main/java/org/futo/voiceinput/MainActivity.kt
+++ app/src/main/java/org/futo/voiceinput/MainActivity.kt
@@ -1,7 +1,8 @@
 package org.futo.voiceinput
 
-import android.content.Context
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.Settings
 import android.speech.RecognizerIntent
@@ -9,72 +10,226 @@ import android.view.inputmethod.InputMethodManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.futo.voiceinput.ui.theme.Slate300
 import org.futo.voiceinput.ui.theme.WhisperVoiceInputTheme
 
+@Composable
+fun useIsInputMethodEnabled(i: Int): MutableState<Boolean> {
+    val enabled = remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
+    LaunchedEffect(i) {
+        val packageName = context.packageName
+        val imm = context.getSystemService(ComponentActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        var found = false
+        for (imi in imm.enabledInputMethodList) {
+            if (packageName == imi.packageName) {
+                found = true
+            }
+        }
+
+        enabled.value = found
+    }
+
+    return enabled
+}
+
+@Composable
+fun useIsMicrophonePermitted(i: Int): MutableState<Boolean> {
+    val permitted = rememberSaveable { mutableStateOf(false) }
+
+    val context = LocalContext.current
+    LaunchedEffect(i) {
+        if(!permitted.value) {
+            permitted.value =
+                context.checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    return permitted
+}
+
+@Composable
+fun SetupContainer(inner: @Composable () -> Unit) {
+    WhisperVoiceInputTheme {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(fraction = 1.0f)
+                    .fillMaxHeight(fraction = 0.4f)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.futo_logo),
+                    contentDescription = "FUTO Logo",
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth(0.75f)
+                        .align(CenterHorizontally),
+                    tint = Slate300
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(fraction = 0.5f)
+                        .align(CenterVertically)
+                        .padding(32.dp)
+                ) {
+                    Box(modifier = Modifier.align(CenterVertically)) {
+                        inner()
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+// TODO: May wish to have a skip option
+@Composable
+@Preview
+fun SetupEnableIME(onClick: () -> Unit = { }) {
+    val context = LocalContext.current
+    val launchImeOptions = {
+        val intent = Intent()
+        intent.action = Settings.ACTION_INPUT_METHOD_SETTINGS
+        intent.addCategory(Intent.CATEGORY_DEFAULT)
+        context.startActivity(intent)
+
+        onClick()
+    }
+
+    SetupContainer {
+        Column {
+            Text(
+                "To integrate with existing keyboards, you need to enable the Voice Input Method.",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Button(
+                onClick = launchImeOptions,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text("Open Input Method Settings")
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun SetupEnableMic(onClick: () -> Unit = { }) {
+    val context = LocalContext.current
+    val askMicAccess = {
+        (context as MainActivity).requestPermission()
+        onClick()
+    }
+
+    SetupContainer {
+        Column {
+            // TODO: Include some privacy statement
+            Text(
+                "In order to use Voice Input, you need to grant microphone permission.",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Button(
+                onClick = askMicAccess,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text("Grant Microphone")
+            }
+        }
+    }
+}
+
+@Composable
+fun SetupSwitch(voiceIntentCallback: () -> Unit = { }, voiceIntentResult: String = "Result goes here", micI: Int = 0) {
+    var i by remember { mutableStateOf(0) }
+
+    val inputMethodEnabled = useIsInputMethodEnabled(i)
+    val microphonePermitted = useIsMicrophonePermitted(i)
+
+    val refresh = {
+        i += 1
+    }
+
+    LaunchedEffect(micI) { refresh() }
+
+    if (!inputMethodEnabled.value) {
+        SetupEnableIME(onClick = refresh)
+    } else if (!microphonePermitted.value) {
+        SetupEnableMic(onClick = refresh)
+    } else {
+        InputTest(voiceIntentCallback, voiceIntentResult)
+    }
+}
 
 class MainActivity : ComponentActivity() {
     private var resultText = "Result goes here"
-    private var needsToEnableIME = true
+    private var micI = 0
     private fun updateContent() {
-        needsToEnableIME = !isThisImeEnabled(
-            this,
-            getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-        )
-
         setContent {
             WhisperVoiceInputTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    InputTest(voiceIntentCallback = {
-                        launchVoiceIntent()
-                    }, voiceIntentResult = resultText,
-                    needToEnableIME = needsToEnableIME,
-                    enableIME = {
-                        invokeLanguageAndInputSettings()
-                    })
+                    SetupSwitch(
+                        voiceIntentCallback = { launchVoiceIntent() },
+                        voiceIntentResult = resultText,
+                        micI = micI
+                    )
                 }
             }
         }
     }
 
-    private fun isThisImeEnabled(
-        context: Context,
-        imm: InputMethodManager
-    ): Boolean {
-        val packageName = context.packageName
-        for (imi in imm.enabledInputMethodList) {
-            if (packageName == imi.packageName) {
-                return true
-            }
-        }
-        return false
+    private val permission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        micI++ // TODO: Kinda hacky
+        updateContent()
     }
 
-    private fun invokeLanguageAndInputSettings() {
-        val intent = Intent()
-        intent.action = Settings.ACTION_INPUT_METHOD_SETTINGS
-        intent.addCategory(Intent.CATEGORY_DEFAULT)
-        startActivity(intent)
+    internal fun requestPermission() {
+        permission.launch(Manifest.permission.RECORD_AUDIO)
     }
 
     private val voiceIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
@@ -106,6 +261,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onResume() {
         super.onResume()
+
+        micI += 1
         updateContent()
     }
 
@@ -117,7 +274,7 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun InputTest(voiceIntentCallback: () -> Unit = { }, voiceIntentResult: String = "Result goes here", needToEnableIME: Boolean = true, enableIME: () -> Unit = {}) {
+fun InputTest(voiceIntentCallback: () -> Unit = { }, voiceIntentResult: String = "Result goes here") {
     var text by remember { mutableStateOf("") }
 
     LaunchedEffect(voiceIntentResult) {
@@ -127,11 +284,6 @@ fun InputTest(voiceIntentCallback: () -> Unit = { }, voiceIntentResult: String =
     }
 
     Column() {
-        if(needToEnableIME) {
-            Button(onClick = { enableIME() }, modifier = Modifier.fillMaxWidth().padding(16.dp)) {
-                Text("The Voice Input Method appears to be disabled. Please enable the Voice Input Method to use integration with AOSP/OpenBoard keyboards")
-            }
-        }
         TextField(
             value = text,
             onValueChange = { text = it },
