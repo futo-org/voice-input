8180d4b764d37f54011cc8160ec4943d9a53fef0
Aleksandras Kostarevas <aleks076@protonmail.com>
2023-06-15 12:44:24 +0300

    Add navigation to testing menu

--- app/src/main/java/org/futo/voiceinput/settings/Hooks.kt
+++ app/src/main/java/org/futo/voiceinput/settings/Hooks.kt
@@ -7,11 +7,20 @@ import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.platform.LocalContext
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import org.futo.voiceinput.Status
+import org.futo.voiceinput.dataStore
 
 
 @Composable
@@ -48,4 +57,32 @@ fun useIsMicrophonePermitted(i: Int): MutableState<Status> {
     }
 
     return permitted
+}
+
+
+
+
+data class DataStoreItem<T>(val value: T, val setValue: (T) -> Job)
+@Composable
+fun <T> useDataStore(key: Preferences.Key<T>, default: T): DataStoreItem<T> {
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
+    val enableSoundFlow: Flow<T> = remember {
+        context.dataStore.data.map {
+                preferences -> preferences[key] ?: default
+        }
+    }
+
+    val value = enableSoundFlow.collectAsState(initial = default).value!!
+
+    val setValue = { newValue: T ->
+        coroutineScope.launch {
+            context.dataStore.edit { preferences ->
+                preferences[key] = newValue
+            }
+        }
+    }
+
+    return DataStoreItem(value, setValue)
 }
--- app/src/main/java/org/futo/voiceinput/settings/SettingsActivity.kt
+++ app/src/main/java/org/futo/voiceinput/settings/SettingsActivity.kt
@@ -9,12 +9,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
@@ -22,30 +30,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.Preferences
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import kotlinx.coroutines.flow.Flow
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.futo.voiceinput.ENABLE_SOUND
 import org.futo.voiceinput.Status
-import org.futo.voiceinput.dataStore
 import org.futo.voiceinput.ui.theme.Typography
 import org.futo.voiceinput.ui.theme.WhisperVoiceInputTheme
 
@@ -77,49 +84,108 @@ class SettingsViewModel : ViewModel() {
 
 
 @Composable
-@Preview
-fun Settings() {
-    val context = LocalContext.current
-    val coroutineScope = rememberCoroutineScope()
-
-    val enableSoundFlow: Flow<Boolean> = remember {
-        context.dataStore.data.map {
-            preferences -> preferences[ENABLE_SOUND] ?: true
+fun SettingItem(title: String, subtitle: String? = null, onClick: () -> Unit, icon: (@Composable () -> Unit)? = null, content: @Composable () -> Unit) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .height(54.dp)
+        .clickable {
+            onClick()
         }
-    }
-
-    val enabled = enableSoundFlow.collectAsState(initial = true).value!!
-
-    val onClick = { value: Boolean ->
-        coroutineScope.launch {
-            context.dataStore.edit { preferences ->
-                preferences[ENABLE_SOUND] = value
-            }
-        }
-    }
-
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(8.dp)) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .clickable {
-                onClick(!enabled)
-            }) {
-            Row(modifier = Modifier
-                .weight(1f)
-                .align(CenterVertically)) {
-                Column {
-                    Text("Sounds", style = Typography.bodyLarge)
-                    Text("Play a sound when recognition is started or cancelled", style = Typography.labelSmall)
+        .padding(0.dp, 4.dp, 8.dp, 4.dp)
+    ) {
+        Column(modifier = Modifier.width(42.dp).align(CenterVertically)) {
+            Box(modifier = Modifier.align(CenterHorizontally)) {
+                if (icon != null) {
+                    icon()
                 }
             }
-            Switch(checked = enabled, onCheckedChange = { onClick(!enabled) })
+        }
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .align(CenterVertically)
+        ) {
+            Column {
+                Text(title, style = Typography.bodyLarge)
+
+                if (subtitle != null) {
+                    Text(subtitle, style = Typography.labelSmall, color = MaterialTheme.colorScheme.outline)
+                }
+            }
+        }
+        Box(modifier = Modifier.align(CenterVertically)) {
+            content()
+        }
+    }
+}
+
+
+@Composable
+fun SettingToggle(title: String, key: Preferences.Key<Boolean>, default: Boolean, subtitle: String? = null, icon: (@Composable () -> Unit)? = null) {
+    val (enabled, setValue) = useDataStore(key, default)
+
+    SettingItem(
+        title = title,
+        subtitle = subtitle,
+        onClick = { setValue(!enabled) },
+        icon = icon
+    ) {
+        Switch(checked = enabled, onCheckedChange = { setValue(!enabled) })
+    }
+}
+
+@Composable
+fun SettingList(content: @Composable () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp, 8.dp)
+    ) {
+        content()
+    }
+}
+
+@Composable
+@Preview
+fun SettingsHome(settingsViewModel: SettingsViewModel = viewModel(), navController: NavHostController = rememberNavController()) {
+    Column(modifier = Modifier.padding(16.dp).fillMaxHeight()) {
+        Text("Settings", style = Typography.titleLarge)
+
+        SettingList {
+            SettingToggle(
+                "Sounds",
+                ENABLE_SOUND,
+                default = true,
+                subtitle = "Play sound when recognition starts/cancels"
+            )
+            SettingItem(title = "Testing Menu", onClick = { navController.navigate("testing") }) {
+                Icon(Icons.Default.ArrowForward, contentDescription = "Go")
+            }
         }
     }
 }
 
 @Composable
+@Preview
+fun SettingsMain(settingsViewModel: SettingsViewModel = viewModel(), navController: NavHostController = rememberNavController()) {
+    val settingsUiState by settingsViewModel.uiState.collectAsState()
+
+    NavHost(
+        navController = navController,
+        startDestination = "settingsHome"
+    ) {
+        composable("settingsHome") {
+            SettingsHome(settingsViewModel, navController)
+        }
+        composable("testing") {
+            InputTest(settingsUiState.intentResultText)
+        }
+    }
+
+}
+
+@Composable
+@Preview
 fun SetupOrMain(settingsViewModel: SettingsViewModel = viewModel()) {
     val settingsUiState by settingsViewModel.uiState.collectAsState()
 
@@ -142,12 +208,7 @@ fun SetupOrMain(settingsViewModel: SettingsViewModel = viewModel()) {
             }
         }
     } else {
-        // TODO: A settings menu instead of straight to InputTest
-        //InputTest(settingsUiState.intentResultText)
-        Column {
-            InputTest(settingsUiState.intentResultText)
-            Settings()
-        }
+        SettingsMain(settingsViewModel)
     }
 }
 
