42bbbb23e46b1c71cb5b5767530328bd5caa1652
Aleksandras Kostarevas <aleks076@protonmail.com>
2023-07-21 23:41:32 +0300

    Update build scripts to check for existence of env variables

--- setUpKeysCI.sh
+++ setUpKeysCI.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo $KEYSTORE_FILE | base64 --decode > key.jks
-echo storePassword=$KEYSTORE_PASSWORD > keystore.properties
-echo keyPassword=$KEY_PASSWORD >> keystore.properties
-echo keyAlias=$KEYSTORE_ALIAS >> keystore.properties
+echo ${KEYSTORE_FILE?Need keystore file} | base64 --decode > key.jks
+echo storePassword=${KEYSTORE_PASSWORD?Need keystore password} > keystore.properties
+echo keyPassword=${KEY_PASSWORD?Need key password} >> keystore.properties
+echo keyAlias=${KEYSTORE_ALIAS?Need key alias} >> keystore.properties
 echo storeFile=key.jks >> keystore.properties
