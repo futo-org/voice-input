52f9314f65def48a5a3e3f1905d98fef45e9221b
Aleksandras Kostarevas <aleks076@protonmail.com>
2023-09-16 19:45:44 +0300

    Update PaymentCompleteActivity with futo pay logic

--- app/src/main/java/org/futo/voiceinput/settings/PaymentCompleteActivity.kt
+++ app/src/main/java/org/futo/voiceinput/settings/PaymentCompleteActivity.kt
@@ -1,13 +1,17 @@
 package org.futo.voiceinput.settings
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -15,7 +19,9 @@ import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.launch
 import org.futo.voiceinput.IS_ALREADY_PAID
 import org.futo.voiceinput.IS_PAYMENT_PENDING
+import org.futo.voiceinput.ValueFromSettings
 import org.futo.voiceinput.dataStore
+import org.futo.voiceinput.payments.StatePayment
 import org.futo.voiceinput.startAppActivity
 import org.futo.voiceinput.ui.theme.WhisperVoiceInputTheme
 
@@ -23,7 +29,6 @@ class PaymentCompleteActivity : ComponentActivity() {
     private fun updateContent() {
         setContent {
             WhisperVoiceInputTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -37,16 +42,51 @@ class PaymentCompleteActivity : ComponentActivity() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    private fun onPaid() {
         lifecycleScope.launch {
-            dataStore.edit { it[IS_ALREADY_PAID] = true }
-            dataStore.edit { it[IS_PAYMENT_PENDING] = false }
+            dataStore.edit {
+                it[IS_ALREADY_PAID] = true
+                it[IS_PAYMENT_PENDING] = false
+            }
 
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 updateContent()
             }
+        }
+    }
+
+    private fun onInvalidKey() {
+        lifecycleScope.launch {
+            if(ValueFromSettings(IS_ALREADY_PAID, false).get(this@PaymentCompleteActivity)) {
+                finish()
+            } else {
+                setContent {
+                    WhisperVoiceInputTheme {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            Text("Invalid license format", modifier = Modifier.padding(8.dp))
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val targetData = intent.dataString
+        if(targetData?.startsWith("futo-voice-input://license/") == true) {
+            if(StatePayment.instance.setPaymentLicenseUrl(targetData)) {
+                onPaid()
+            } else {
+                onInvalidKey()
+            }
+        } else {
+            Log.e("PaymentCompleteActivity", "futo-voice-input launched with invalid targetData $targetData")
+            onInvalidKey()
         }
     }
 
