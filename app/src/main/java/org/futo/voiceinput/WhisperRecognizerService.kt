161333c5f68115215ca31730fc9cbbd017daa6f2
Aleksandras Kostarevas <aleks076@protonmail.com>
2023-10-21 17:40:37 +0300

    Recognition service stub

---
+++ app/src/main/java/org/futo/voiceinput/WhisperRecognizerService.kt
@@ -0,0 +1,19 @@
+package org.futo.voiceinput.WhisperVoiceInput
+
+import android.app.Service
+import android.content.Intent
+import android.speech.RecognitionService
+
+class WhisperRecognizerService : RecognitionService() {
+    override fun onStartListening(p0: Intent?, p1: Callback?) {
+
+    }
+
+    override fun onCancel(p0: Callback?) {
+
+    }
+
+    override fun onStopListening(p0: Callback?) {
+        
+    }
+}
